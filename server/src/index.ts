9cfb325784fa4e54798de7902272a5a77a7faf6b
Alvaro Parker <aparkerdf@protonmail.com>
2024-10-29 12:37:12 -0300

    Small fix

--- server/src/index.ts
+++
@@ -1,8 +0,0 @@
-import server from "./server";
-
-// Server contruction and deployment
-// FIXME change console log to show actual working port
-const port = process.env.PORT || 4000;
-server.listen(port, () => {
-	console.log("API Up");
-});
